6cacbebe2c7d3fe4f3936980dea4381e7938781e
Eduardo <eduardoarevaloportero@gmail.com>
2025-03-21 10:30:35 +0100

    DHT22 con los cambios para conectarse a raspberry

--- api/lectura_endpoints.py
+++ api/lectura_endpoints.py
@@ -1,9 +1,9 @@
 # api/lectura_endpoints.py
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from metodos.sensor_crud import insertar_sensor
-from metodos.medicion_crud import insertar_medicion
-from metodos.registro_sensor_crud import insertar_registro_sensor
+from metodos.sensor_crud import create_sensor
+from metodos.medicion_crud import create_medicion
+from metodos.registro_sensor_crud import create_registro_sensor
 from sensors.dht22 import DHT22
 from sensors.bmp180 import BMP180
 from sensors.mq135 import MQ135
@@ -23,19 +23,19 @@ def simular_lecturas(db: Session = Depends(get_db)):
     calidad_aire = sensor_mq135.leer_calidad_aire()
 
     # Insertar sensores si no existen
-    sensor_dht22 = insertar_sensor(db, "DHT22")
-    sensor_bmp180 = insertar_sensor(db, "BMP180")
-    sensor_mq135 = insertar_sensor(db, "MQ135")
+    sensor_dht22 = create_sensor(db, "DHT22")
+    sensor_bmp180 = create_sensor(db, "BMP180")
+    sensor_mq135 = create_sensor(db, "MQ135")
 
     # Insertar mediciones
-    medicion_dht22 = insertar_medicion(db, temperatura=temperatura, humedad=humedad)
-    medicion_bmp180 = insertar_medicion(db, presion=presion)
-    medicion_mq135 = insertar_medicion(db, calidad_aire=calidad_aire)
+    medicion_dht22 = create_medicion(db, temperatura=temperatura, humedad=humedad)
+    medicion_bmp180 = create_medicion(db, presion=presion)
+    medicion_mq135 = create_medicion(db, calidad_aire=calidad_aire)
 
     # Insertar registros de sensores
-    insertar_registro_sensor(db, sensor_id=sensor_dht22.id, medicion_id=medicion_dht22.id, tipo_medicion="temperatura", valor=temperatura)
-    insertar_registro_sensor(db, sensor_id=sensor_dht22.id, medicion_id=medicion_dht22.id, tipo_medicion="humedad", valor=humedad)
-    insertar_registro_sensor(db, sensor_id=sensor_bmp180.id, medicion_id=medicion_bmp180.id, tipo_medicion="presion", valor=presion)
-    insertar_registro_sensor(db, sensor_id=sensor_mq135.id, medicion_id=medicion_mq135.id, tipo_medicion="calidad_aire", valor=calidad_aire)
+    create_registro_sensor(db, sensor_id=sensor_dht22.id, medicion_id=medicion_dht22.id, tipo_medicion="temperatura", valor=temperatura)
+    create_registro_sensor(db, sensor_id=sensor_dht22.id, medicion_id=medicion_dht22.id, tipo_medicion="humedad", valor=humedad)
+    create_registro_sensor(db, sensor_id=sensor_bmp180.id, medicion_id=medicion_bmp180.id, tipo_medicion="presion", valor=presion)
+    create_registro_sensor(db, sensor_id=sensor_mq135.id, medicion_id=medicion_mq135.id, tipo_medicion="calidad_aire", valor=calidad_aire)
 
     return {"message": "Lecturas simuladas y almacenadas con éxito"}
