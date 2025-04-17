80a7dbb42f2002011642ed3d326d373e61da4661
Eduardo <eduardoarevaloportero@gmail.com>
2025-04-17 13:41:51 +0200

    anotar simulacion sensores

--- sensors/bmp180.py
+++ sensors/bmp180.py
@@ -1,8 +1,10 @@
 import random
 
+#Utilizada en la primera fase para simluar registros. Ahora no tiene funcionalidad
+
 class BMP180:
     def leer_temperatura(self):
-        return round(random.uniform(15, 25), 2)  # Simulación de temperatura
+        return round(random.uniform(15, 25), 2)
 
     def leer_presion(self):
-        return round(random.uniform(900, 1100), 2)  # Simulación de presión
+        return round(random.uniform(900, 1100), 2)
